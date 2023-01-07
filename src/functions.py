df04607b1a0e7c91228fe86a441a2c2a6fc9b525
Vênus <58833700+zilohnoji@users.noreply.github.com>
2023-01-06 23:12:53 -0300

    Add files via upload

--- src/functions.py
+++ src/functions.py
@@ -1,1 +1,15 @@
+from time import sleep
+from sys import stdout
 
+class Configs:
+    def __init__(self) -> None:
+        pass
+
+    def animation(var: str) -> str:
+        list_var = [broken for broken in var]
+        for n in list_var:
+            stdout.write(n)
+            stdout.flush()
+            sleep(.1)
+
+obj_class = Configs.animation("Pedro Donato")
