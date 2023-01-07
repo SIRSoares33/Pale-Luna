0c7ad8c1bf306edd035ea0770b1135d0883d25bb
Vênus <58833700+zilohnoji@users.noreply.github.com>
2023-01-06 23:30:05 -0300

    Add files via upload

--- src/functions.py
+++ src/functions.py
@@ -2,14 +2,25 @@ from time import sleep
 from sys import stdout
 
 class Configs:
+    __slots__ = ("colors")
+    
     def __init__(self) -> None:
-        pass
+        self.colors = {"red": "\033[01;31m",
+                        "white": "\033[1;37m",
+                        "endcolor": "\033[0m"
+                        }
 
-    def animation(var: str) -> str:
+    def animation(self, var: str) -> str:
+        if var == "You Lost":
+            color = self.colors["red"]
+        else:
+            color = self.colors["white"]
+
         list_var = [broken for broken in var]
         for n in list_var:
-            stdout.write(n)
+            stdout.write(f"{color}{n}{self.colors['endcolor']}")
             stdout.flush()
             sleep(.1)
 
-obj_class = Configs.animation("Pedro Donato")
+obj_class = Configs()
+obj_class.animation("You Lost")
